4ba0ead44f200b259de43d48efb8080c7cccdfa6
God-Of-Geeks <godofgeeeks@gmail.com>
2021-10-09 20:45:58 -0500

    srik

--- Transaction.java
+++ Transaction.java
@@ -1,37 +1,38 @@
-import java.io.BufferedReader;  
-import java.io.FileReader;  
-import java.io.IOException;   
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.*;
-public class Transaction { 
-	public String tx_hash;	
-	public String from_addr;	
+
+public class Transaction { // First class
+	public String tx_hash;
+	public String from_addr;
 	public String to_addr;
-	public String block_number;	
-	public String token_qty;	
-	public String block_time;	
-	public String block_hash;	
-	public String gas;	
-	public String gas_price;	
-	public String tx_index_in_block;	
+	public String block_number;
+	public String token_qty;
+	public String block_time;
+	public String block_hash;
+	public String gas;
+	public String gas_price;
+	public String tx_index_in_block;
 	public String total_gas;
-	
-	Transaction(String tx_hash,
+
+	Transaction(String tx_hash, // Constructor initialization
 	String from_addr,
 	String to_addr,
-	String block_number,	
+	String block_number,
 	String token_qty,
-	String block_time,	
-	String block_hash,	
-	String gas,	
+	String block_time,
+	String block_hash,
+	String gas,
 	String gas_price,
-	String tx_index_in_block,	
+	String tx_index_in_block,
 	String total_gas){
-		this.tx_hash = tx_hash; 
-		this.from_addr = from_addr; 
-		this.to_addr = to_addr; 
+		this.tx_hash = tx_hash;
+		this.from_addr = from_addr;
+		this.to_addr = to_addr;
 		this.block_number = block_number;
 		this.token_qty = token_qty;
-		this.block_time = block_time; 
+		this.block_time = block_time;
 		this.block_hash = block_hash;
 		this.gas = gas;
 		this.gas_price = gas_price;
@@ -39,33 +40,33 @@ public class Transaction {
 		this.total_gas = total_gas;
 
 	}
-	
-	public static void main(String[] args)    
-	{  
+
+	public static void main(String[] args)
+	{
 		//Transaction t = new Transaction();
-		String line = "";  
-		String splitBy = ",";  
+		String line = "";
+		String splitBy = ",";
 		List<Transaction> tS = new ArrayList<Transaction>();
-		try   
-		{  
+		try
+		{
 		int count =0;
-		BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\DELL\\Desktop\\MS Sem1\\DS\\Project1\\input_data.csv"));  
-		while ((line = br.readLine()) != null)   
-		{  
+		BufferedReader br = new BufferedReader(new FileReader("/Users/srikaramara/Git/ds/transactions.csv"));
+		while ((line = br.readLine()) != null)
+		{
 		String[] employee = line.split(splitBy);
 		for(String s : employee) {
 		System.out.println(s);
-		//Transaction t = new Transaction();
+		//Transaction t = new Transaction("");
 		}
 
 		count++;
 		System.out.println(count);
-		}  
-		}   
-		catch (IOException e)   
-		{  
-		e.printStackTrace();  
-		}  
-		
+		}
+		}
+		catch (IOException e)
+		{
+		e.printStackTrace();
+		}
+
 	}
 }
